8c302bcce862cc0ef143cb02a0894461b3d1bb62
DEV TYAGI <devtg3@gmail.com>
2019-10-27 23:48:07 +0530

    week 6 added

---
+++ course2/week6.py
@@ -0,0 +1,16 @@
+name = input("Enter file:")
+if len(name) < 1 : name = "mbox-short.txt"
+handle = open(name)
+ls=dict()
+for line in handle:
+    line.rstrip()
+    if line.startswith('From'):
+        words=line.split()
+        if len(words)>6:
+            time=words[5]
+            hrs=time.split(':')
+            hr=hrs[0]
+            ls[hr]=ls.get(hr,0)+1
+ls=sorted(ls.items())
+for x,y in ls:
+    print(x,y)
